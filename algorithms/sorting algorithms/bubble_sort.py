0c59a4fc0028a402b82137e7eaa8280113ef6c45
zheer barzan <zheerbarzan85@outlook.com>
2024-08-18 16:33:43 +0300

    adding bubble sort algoritm

---
+++ algorithms/sorting algorithms/bubble_sort.py
@@ -0,0 +1,17 @@
+numbers = [5,4,3,2,1]
+
+
+def bubble_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j]> arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+
+print(bubble_sort(numbers))
+
+
+

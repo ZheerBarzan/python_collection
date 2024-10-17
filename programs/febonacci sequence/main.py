5d31dd1d72be0a2d3f6b9cc1557649a036742593
Zheer Barzan <zheerbarzan85@outlook.com>
2024-10-18 01:21:39 +0300

    optimized

--- programs/febonacci sequence/main.py
+++ programs/febonacci sequence/main.py
@@ -1,14 +1,17 @@
 
-
+memo = {}
 def feb(n):
+    if n in memo:
+        return memo[n]
 
     if n <=2:
         result = 1
     else:
         result = feb(n -1) + feb(n-2)
 
-    return result
+    memo[n] = result
 
+    return result
 
 print(feb(7))
 print(feb(50))

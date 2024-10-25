af1b2216e203afe17f0e2db507641e2952783c76
zheer barzan <zheerbarzan85@outlook.com>
2024-10-25 20:49:03 +0300

    swapping without a 3rd variable

---
+++ problems_in_python/swapping without swapping/swap.py
@@ -0,0 +1,8 @@
+a = 0b1010
+b = 0b0011
+print('a=',bin(a),' b=', bin(b)) # 0b1010 0b0011
+a = a^b
+b = a^b
+print('a=',bin(a),' b=', bin(b)) # 0b1010 0b0011
+a = a^b
+print('a=',bin(a),' b=', bin(b)) # 0b1010 0b0011

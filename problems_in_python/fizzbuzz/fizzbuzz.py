b6d254430c50b892dcfd29261cf1e2b5bcaf7457
zheer barzan <zheerbarzan85@outlook.com>
2025-01-28 22:46:05 +0300

    adding the fizzbuzz problem

---
+++ problems_in_python/fizzbuzz/fizzbuzz.py
@@ -0,0 +1,11 @@
+numbers = int(input("Enter a number: "))
+
+for number in range(1,numbers+1):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
